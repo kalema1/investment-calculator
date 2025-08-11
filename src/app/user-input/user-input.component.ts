8e4095c6be011d1a27ea5931ca0a6a2a5d2d8f41
kalema1 <kalemar8@gmail.com>
2025-08-11 20:09:03 +0300

    add logic to the user input component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -13,11 +13,42 @@ export class UserInputComponent {
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
+
   onSubmit() {
     console.log('submitted');
     console.log(this.enteredInitialInvestment);
     console.log(this.enteredAnnualInvestment);
     console.log(this.enteredExpectedReturn);
     console.log(this.enteredDuration);
+  }
+
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
   }
 }
